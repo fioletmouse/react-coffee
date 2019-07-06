bcd3e4f34af5af2e78596c476bd04d5123651093
Evgeniia <e.akulenko@velvetech.ru>
2019-07-06 18:46:13 +0700

    started to customize the project

---
+++ src/data.js
@@ -0,0 +1,81 @@
+export default[
+  {
+    code: 'espresso',
+    name: 'Espresso',
+    image: 'espresso.png',
+    info: "For some purists, espresso is the king of coffees and the only true way to drink the stuff. The espresso is where it all \n\
+    starts: without espresso, none of the other drinks on this list would be possible. Espresso is made by forcing pressurized \n\
+    hot water at a temperature of about 190-200°F (88-93°C) through a small amount of finely ground beans. The result is a thick, \n\
+    rich and intense shot of coffee with a golden “crema” on top. Many people believe espresso has a higher caffeine content \n\
+    than other coffees. This is true if you are measuring by volume – but there is a higher dose of caffeine in a regular cup\n\
+     of drip coffee or coffee made with a French press since there is simply more of it. There are many factors to consider \n\
+     when making espresso, including beans, grind, amount of coffee, time, temperature and pressure. Check out this video to see \n\
+     how they all combine to make the perfect shot."    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  },
+  {
+    code: '',
+    name: ''
+    image: '',
+    info: ''    
+  }
+]
