129785c12f080ff15238848791fcb38545a00cb3
Evgeniia <e.akulenko@velvetech.ru>
2019-07-06 19:01:00 +0700

    full data

--- src/data.js
+++ src/data.js
@@ -13,69 +13,120 @@ export default[
      how they all combine to make the perfect shot."    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'doppio',
+    name: 'Doppio',
+    image: 'Doppio.png',
+    info: 'On some menus, you may see the word “doppio”, which is just Italian for double. A doppio is simply a double espresso, \n\
+    two regular espressos in one cup. A doppio should definitely not mean the barista just allows more water to pass into the cup \n\
+    when pulling the espresso, this will only result in a more diluted drink (see “lungo” below) and not a true “doppio”. \n\
+    Sometimes you may see “doppio” with other espresso drinks – this means the drink you are ordering is a double-strength \n\
+    version with two espresso shots in it instead of just one.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'ristretto',
+    name: 'Ristretto',
+    image: 'ristretto.png',
+    info: 'When serving a standard espresso, most of the coffee is extracted in the first moments, after which the liquid coming out \n\
+    becomes progressively more diluted. Part of the art of pulling a perfect espresso means knowing how long to wait before stopping \n\
+    the flow. As mentioned above, if the barista allows the water to keep flowing too long, the espresso will become too diluted. \n\
+    On the other hand, if the barista stops sooner, the coffee will be even thicker and more intense than a standard espresso: \n\
+    this is a ristretto, a stronger, more concentrated espresso. If you want the shortest, blackest, strongest shot of coffee \n\
+    available, ask for a ristretto – but don’t be surprised if you don’t find very much coffee in your cup.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'americano',
+    name: 'Americano',
+    image: 'americano.png',
+    info: 'As the legend goes, the Americano was created during the Second World War when American soldiers were sent to fight in \n\
+    Europe. When soldiers in Italy ordered a coffee, they expected something like back home, but instead, they were served espressos, \n\
+    not what they wanted at all. Using what was available, the local baristas took their espressos and added hot water to make \n\
+    a long drink more suited to the G.I.s’ tastes. The Americano was born. The Americano is sometimes somewhat derided. \n\
+    Why would you want to water down the delicious intensity of an espresso? But if you’re in the mood for something that \n\
+    lasts a bit longer than a 1oz shot and you don’t want a milky concoction like a latte or cappuccino, an Americano could \n\
+    be the one to go for.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'cappuccino',
+    name: 'Cappuccino',
+    image: 'cappuccino.png',
+    info: 'The cappuccino is one of the all-time classic espresso-based drinks. The modern cappuccino comes from Italy, where it is\n\
+     commonly enjoyed with breakfast. In the land of its birth, it is unusual – and considered slightly strange – to order one after \n\
+     about 11 am. A cappuccino is made by first pulling an espresso shot (or a double) into a round cup. The barista then uses \n\
+     the steam wand on the espresso machine to heat a jug of milk, which separates into the froth and heated milk. This mixture \n\
+     is poured onto the espresso shot, creating a milky coffee drink topped by a layer of froth. The ratio of espresso to milk \n\
+     to froth should be about 1:1:1. Skilled baristas often decorate cappuccinos with elaborate patterns in the froth, now known \n\
+     as “latte art”.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'latte',
+    name: 'Latte',
+    image: 'latte.png',
+    info: 'The latte has become one of the most popular espresso-based drinks all around the world, largely due to the influence of \n\
+    chains like Starbucks and the like. Some people may be confused about the difference between a cappuccino and a latte since \n\
+    they are made in a fairly similar way – but they are quite distinct. As with cappuccinos, lattes are made by adding steamed \n\
+    milk to an espresso shot. However, the difference lies in the ratios. With a latte, there is much more milk compared \n\
+    to the espresso, and the layer of foam on the top is much thinner than with a cappuccino. Lattes are often served in \n\
+    tall glasses, although sometimes they may be presented in a cappuccino-style cup. Baristas often decorate lattes with latte \n\
+    art, hence the name of this practice. Caffè latte in Italian literally means “milk coffee”. If you just ask for a latte \n\
+    in a bar in Italy, you will probably be served a glass of hot milk!'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'flatWhite',
+    name: 'Flat White',
+    image: 'flat-white.png',
+    info: 'Although it has been a popular drink in Australia and New Zealand for many decades, the flat white has recently burst \n\
+    onto the international coffee scene, rapidly establishing itself as the coffee of choice for hipsters the world over. If \n\
+    you’re still struggling to understand the difference between a cappuccino and a latte, the flat white is only going to \n\
+    confuse you even more. Some might consider it something like a hybrid of the two, others will simply dismiss it as a \n\
+    “small latte”. However, it’s neither of these. It’s something entirely different. Maybe. As with cappuccinos and lattes, \n\
+    flat whites are made by adding steamed milk to an espresso shot. The difference here is that the milk is not separated \n\
+    into milk and froth; rather, the whole volume of milk should be in the form of microfoam. This means when the milk is \n\
+    poured into the espresso, it combines completely with the coffee, leaving no layer of foam on top. Since flat whites are \n\
+    usually made with a double shot of espresso and a lower volume of milk than lattes or cappuccinos, the resulting drink is \n\
+    less milky and with a stronger espresso flavor.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'mocha',
+    name: 'Mocha',
+    image: 'mocha.png',
+    info: 'Depending on how you see things, a mocha is either a luxurious, indulgent treat for when you want to spoil yourself or \n\
+    alternatively, it’s a coffee drink for people who don’t really like coffee. In its most basic form, a mocha is nothing more \n\
+    than a hot chocolate with a shot of espresso thrown in. However, most coffee shops will give you something much more \n\
+    elaborate more along the lines of a latte with chocolate inside and maybe cream on top. The chocolate itself can be \n\
+    in the form of hot chocolate powder or real melted chocolate. The chocolate used is usually milk chocolate, but white or \n\
+    dark chocolate can be used instead. Be careful not to confuse mocha the espresso drink with mocha (a type of coffee bean), \n\
+    Mocha (the place in Yemen where mocha beans come from) or moka (same pronunciation), the traditional pot that is especially \n\
+    popular for making coffee in Italy and Spain.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'macchiato',
+    name: 'Macchiato',
+    image: 'macchiato.png',
+    info: 'There is some contention over the precise definition of a macchiato so let’s clear things up right now. Macchiato in \n\
+    Italian means “marked” or “stained” and a caffè macchiato is simply an espresso “marked” with a dash of milk. However, \n\
+    certain coffee shops, including some large and well-known chains, seem to have misappropriated this name, evidently reasoning \n\
+    that giving a coffee drink an Italian-sounding name will make it sell better. You will now find all manner of beverages being \n\
+    served under this moniker. Sometimes macchiatos are “marked” with a dollop of foam – which is not so far from the authentic \n\
+    drink – and other times you will come across drinks sharing nothing with the original beyond the name. It’s not a macchiato \n\
+    just because you call it one…'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'corretto',
+    name: 'Corretto',
+    image: 'corretto.png',
+    info: 'This is not a drink you are likely to find in your local Starbucks, but if you are eating in a decent authentic \n\
+    Italian restaurant, you may be able to try a traditional “corretto” at the end of your meal. Corretto in Italian means \n\
+    “corrected”, and this refers to the fact that the espresso shot has been “corrected” with a shot of strong alcohol, most \n\
+    traditionally Italian grappa (although you are free to “correct” your coffee with the liquor of your choosing. Brandy is also popular.'    
   },
   {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
-  },
-  {
-    code: '',
-    name: ''
-    image: '',
-    info: ''    
+    code: 'raf',
+    name: 'Raf Coffee',
+    image: 'raf.png',
+    info: 'The main feature of raf coffee is that cream is whipped together with espresso and sugar, so the drink becomes thick \n\
+    and tender. Extra ingredients include syrups, milk, brandy, caramel, chocolate, etc. Raf coffee is usually served in cappuccino \n\
+    or latte cups. Raf coffee is one of the ambivalent and disputable drinks. On the one hand, it does not support the philosophy \n\
+    of coffee as a berry and is more like a dessert. On the other hand, it develops the idea of a coffee shop that is cosy, \n\
+    welcoming and ready to be individual.'    
   }
 ]
