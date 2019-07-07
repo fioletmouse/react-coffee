49579f0524f627111586deee5d1b2f2fd59b8468
Evgeniia <e.akulenko@velvetech.ru>
2019-07-07 14:13:06 +0700

    added styles

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Cloud from '../cloud/Cloud';
+import Cloud from '../cloud/Cloud';
 import Search from '../search/Search';
 import Header from '../header/Header';
 import CoffeeList from '../list/List'
@@ -10,7 +10,7 @@ function App() {
       <Header/>
       <Search /> 
       <CoffeeList />
-      {/*<Cloud /> */}
+      <Cloud />
     </div>
   );
 }
--- src/cloud/Cloud.js
+++ src/cloud/Cloud.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import TagCloud from 'react-tag-cloud';
+import styles from './Cloud.module.css';
 
 class Cloud extends Component {
   render() {
     return (
-      <div>
-        <TagCloud 
-        >
-       <div>GI Joe</div>
+      <div className={styles.appOuter}>
+        <div className={styles.appInner}>
+          <TagCloud  className={styles.tagCloud}>
+            <div>GI Joe</div>
             <div>Inspector Gadget</div>
             <div>Bugs Bunny</div>
-      </TagCloud>
+          </TagCloud>    
+        </div>
       </div>
-     
+      
+    
     );
   }
 }
