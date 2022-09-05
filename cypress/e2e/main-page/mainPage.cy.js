8fdba3ab1b352c0039bc68583b09da09ad4d1a2c
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-05 14:27:08 +0300

    update tests

--- cypress/e2e/main-page/mainPage.cy.js
+++ cypress/e2e/main-page/mainPage.cy.js
@@ -1,9 +1,19 @@
+
 describe('main page spec', () => {
-  it('load main page', () => {
+  beforeEach(() => {
     cy.visit('/')
   })
 
   it('have 4 links',  () => {
     cy.get("a").should('have.length', 4)
+    cy.get("a[href='/tools']").should("exist").should("have.text", "tools")
+    cy.get("a[href='/blends']").should("exist").should("have.text", "blends")
+    cy.get("a[href='/recipes']").should("exist").should("have.text", "recipes")
+    cy.get("a[href='/other']").should("exist").should("have.text", "other")
+  })
+
+  it('open tools page',  () => {
+    cy.contains("tools").click()
+    cy.url().should('include', '/tools')
   })
 })
--- src/main-page/main-block/MainBlock.jsx
+++ src/main-page/main-block/MainBlock.jsx
@@ -5,7 +5,7 @@ import style from './MainBlock.module.css';
 function MainBlock(props) {
   return (
     <div className={style.block}>
-      <Link to={`${props.addr}`} className={style.block}>{props.addr} </Link>
+      <Link to={`${props.addr}`} className={style.block}>{props.addr}</Link>
     </div>
   
   );
