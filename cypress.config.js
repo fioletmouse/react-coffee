19b1b5771596aad72d6f47cd4abf55d2a363aa9e
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-05 13:07:59 +0300

    initial test

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  
   e2e: {
+    baseUrl: "http://localhost:3000",
+    video: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
