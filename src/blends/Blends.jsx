8cb44175a758482aeff312017d0ecc1f4fb11ce0
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-06 14:11:48 +0300

    add initil header

--- src/blends/Blends.jsx
+++ src/blends/Blends.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../shared/header/Header';
 
 function Blends() {
   return (
-    <> Not implemented yet </>
+    <>
+      <Header />
+      Not implemented yet
+    </>
   );
 }
 
--- src/other/Other.jsx
+++ src/other/Other.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Header from '../shared/header/Header';
 
 function Other() {
   return (
-    <> Not implemented yet </>
+    <>
+      <Header />
+      Not implemented yet
+    </>
   );
 }
 
