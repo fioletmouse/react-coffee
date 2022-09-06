d7c2b786ba9b8fdbcc06b9a926c842ba56fc8cc0
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-06 14:09:47 +0300

    clear code

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -13,18 +13,9 @@ function Header() {
             className={({ isActive }) => (`${isActive ? 'selected' : ''} col-2 header_btn`)}
           >
             {page.name}
-
           </NavLink>
         ))
       }
-
-      {/* <div className="offset-3 col-6">
-        <header className="text-center">
-          <h2 className="header">
-            Test-e-e-e-e Coffee
-          </h2>
-        </header>
-      </div> */}
     </div>
 
   );
