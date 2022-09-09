45bf36700a3871f025e643db68159fe7869cf9de
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-09 17:47:43 +0300

    add methods

---
+++ src/services/dict-handler.js
@@ -0,0 +1,25 @@
+function DictActions() {
+  let processing = null;
+  const getProcessing = async () => processing;
+  const addProcessing = async (value) => {
+    if (!processing) processing = [];
+    processing.push({ id: Math.random(), name: value });
+  };
+  const updateProcessing = async (id, value) => {
+    if (!processing) throw new Error('no data');
+    const itemRef = processing.find((item) => item.id === id);
+    itemRef.name = value;
+  };
+
+  const deleteProcessing = async (id) => {
+    processing.filter((item) => item.id !== id);
+  };
+
+  return {
+    getProcessing,
+    addProcessing,
+    updateProcessing,
+    deleteProcessing,
+  };
+}
+export default DictActions;
