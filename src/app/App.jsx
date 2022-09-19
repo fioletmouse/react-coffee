b95eefaa304d13cba47044df77970882c0d006f9
Evgeniia Akulenko <e.akulenko@velvetech.ru>
2022-09-19 13:10:35 +0300

    remove tools menu

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -6,13 +6,11 @@ import pages from './pagesList';
 import Blends from '../blends/Blends';
 import CoffeeList from '../coffee-list/CoffeeList';
 import Dict from '../dict/Dict';
-import Tools from '../tools/Tools';
 
 function App() {
   return (
     <Routes>
       <Route exact path="/" element={<MainPage pages={pages} />} />
-      <Route path="/tools" element={<Tools />} />
       <Route path="/blends" element={<Blends />} />
       <Route path="/recipes" element={<CoffeeList />} />
       <Route path="/dict" element={<Dict />} />
--- src/app/pagesList.js
+++ src/app/pagesList.js
@@ -1,5 +1,4 @@
 export default [
-  { name: 'tools', path: 'tools' },
   { name: 'blends', path: 'blends' },
   { name: 'recipes', path: 'recipes' },
   { name: 'dicts', path: 'dict' },
